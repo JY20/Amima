666522a6264cb928ef10cf5b4928e0cbb912b2e1
Jimmy Yan <jimmyyan1337@gmail.com>
2021-06-24 19:26:41 -0400

    UI updated logo added

--- src/landingPage.js
+++ src/landingPage.js
@@ -1,34 +1,39 @@
 import React from "react";
 import logo from './logo.svg';
+import LogoA from './logoAS.jpg';
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 
 export class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.t1 = "   Coming Soon   ";
+  }
   render() {
     return (
       <div style={{
-        // backgroundColor: "#ffeceb"
-        backgroundImage: 'url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fsimple-background-straight-gray-lines-vector-2815528&psig=AOvVaw1FUWqjYAemKXTTMKrqDVbo&ust=1624588182548000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCMCGmuGcr_ECFQAAAAAdAAAAABAK")'
-        , height: "500px"
+        backgroundColor: "#dcedff"
       }}>
-        <p style={{ height: "50px" }}> </p>
-        <img src={logo} style={{ height: "450px", float: "right", border: "1px solid red" }} alt="" />
-        <h1 style={{ fontSize: "50px", float: "left", paddingLeft: "300px", paddingTop: "20px" }}>Amima</h1>
-        <h2 style={{ fontSize: "22px", float: "left", paddingLeft: "120px" }}>
-          This is hellllslslslslssssssssssssssssssssssssss  sadas
-        </h2>
+        <header style={{ fontSize: "80px", paddingTop: "70px",fontWeight: 'bold'}}>Amima</header>
+        <p style={{ fontSize: "35px"}}>
+          Life for a uni student
+        </p>
+        <button style={{border:"none",width:"200px",height:"35px",borderRadius:50, fontSize: "25px", color:"#a30316", backgroundColor:"#f9cacf"}}>
+        {this.t1}
+        </button>
+        <p style={{ height: "50px", fontWeight: 'bold'}}> </p>
         <form>
           <div class="form-group">
-            <label style={{ height: "50px", paddingLeft: "150px", float: "left", fontSize: "24px" }}></label>
-            <input style={{ height: "50px", paddingLeft: "50px", float: "left", fontSize: "24px" }} placeholder="Enter email"></input>
-            <button style={{ height: "56px", float: "left", fontSize: "24px" }}>
-              Submit
+            <label style={{ height: "50px", fontSize: "24px" }}></label>
+            <input style={{borderRadius:50,outline:"none",border:"none",height: "50px",  fontSize: "24px" }} placeholder="Enter email"></input>
+            <button style={{ borderRadius:50, border:"none",height: "56px", fontSize: "24px",backgroundColor:"#80b8fb"}}>
+              Sign Up
             </button>
           </div>
         </form>
-        <p style={{ height: "50px", padding: "100px", margin: "0", }}> </p>
+        <p style={{ height: "50px", padding: "100px", margin: "0"}}> </p>
       </div>
     );
   }
