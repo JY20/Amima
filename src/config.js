56277841897fd3cfb5a1b0232ab0e1bb97923c8f
Jimmy Yan <jimmyyan1337@gmail.com>
2021-06-25 20:24:16 -0400

    footer problem solved

---
+++ src/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const config = {
+  apiKey: "AIzaSyAmvOu9Yn7yh8WHVw8nBmeL39YFDLERs2k",
+  authDomain: "amima-5a62b.firebaseapp.com",
+  projectId: "amima-5a62b",
+  storageBucket: "amima-5a62b.appspot.com",
+  messagingSenderId: "166679900914",
+  appId: "1:166679900914:web:486f49c1b50ad0014a566b",
+  measurementId: "G-XBBLSCJN8L"
+};
+
+function initFirebase() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+}
+
+initFirebase();
+
+export { firebase };
--- src/landingPage.js
+++ src/landingPage.js
@@ -1,15 +1,32 @@
-import React from "react";
-import logo from './logo.svg';
-import LogoA from './logoAS.jpg';
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
-} from 'reactstrap';
+import React, { useState, useEffect } from "react";
+import { firebase } from "./config";
 
+const db = firebase.database();
 export class LandingPage extends React.Component {
   constructor(props) {
     super(props);
     this.t1 = "   Coming Soon   ";
+    this.email ="";
+    this.state = {
+      speed: 10
+    };
+    this.onInputchange = this.onInputchange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  onInputchange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  onSubmitForm() {
+    firebase.database().ref('users/').set({
+      username: 'aas',
+      email: 's',
+      profile_picture : ';'
+    });
+    console.log(this.state+"s")
+    console.log("ssss")
   }
   render() {
     return (
@@ -23,17 +40,30 @@ export class LandingPage extends React.Component {
         <button style={{border:"none",width:"200px",height:"35px",borderRadius:50, fontSize: "25px", color:"#a30316", backgroundColor:"#f9cacf"}}>
         {this.t1}
         </button>
-        <p style={{ height: "50px", fontWeight: 'bold'}}> </p>
+        <p style={{ height: "20px"}}> </p>
+        <p>Sign up to recieve the newest updates for Amima</p>
         <form>
           <div class="form-group">
             <label style={{ height: "50px", fontSize: "24px" }}></label>
-            <input style={{borderRadius:50,outline:"none",border:"none",height: "50px",  fontSize: "24px" }} placeholder="Enter email"></input>
-            <button style={{ borderRadius:50, border:"none",height: "56px", fontSize: "24px",backgroundColor:"#80b8fb"}}>
+            <input style={{borderRadius:50,outline:"none",border:"none",height: "50px",  fontSize: "24px" }} 
+            placeholder="Enter email" value={this.state.lname} onChange={this.onInputchange}></input>
+            <button onClick={this.onSubmitForm}
+            style={{ borderRadius:50, border:"none",height: "56px", fontSize: "24px",backgroundColor:"#80b8fb"}}>
               Sign Up
             </button>
           </div>
         </form>
-        <p style={{ height: "50px", padding: "100px", margin: "0"}}> </p>
+        <p style={{ height: "20px"}}> </p>
+        <div>
+          <label style={{fontSize:"24px",float:"left",fontWeight: 'bold', paddingLeft:"350px"}}>Buy as a group</label>
+          <label style={{fontSize:"24px", float:"left", paddingLeft:"100px"}}>Buying as a group can help to you to save money</label>
+        </div>
+        <p style={{ height: "20px"}}> </p>
+        <footer>
+          <p style={{ height: "20px"}}> </p>
+          <p>Contact Us:</p>
+          <p style={{ height: "20px"}}> </p>
+        </footer>
       </div>
     );
   }
